ca399a8964019a4fc2054d8b2475735bbe31906a
Marcus Christiansen <marcus@firingsquad.co.za>
2018-02-22 13:13:32 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+
+    console.log(`${now}: ${req.method} ${req.url}`)
+    next();
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        content: 'This is some random content',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
